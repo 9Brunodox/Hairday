c9391d617c93ef92655de804f6237acf03fd24ab
9Brunodox <bruno.moreiradasilva.2002@gmail.com>
2025-07-06 01:28:07 -0300

    Renderizando os agendamentos do dia

--- src/services/schedule-new.js
+++ src/services/schedule-new.js
@@ -1,18 +1,19 @@
-import { apiConfig } from "./api-config.js"
+import { apiConfig } from "./api-config.js";
 
-export async function scheduleNew({ id, name, when }){
-    try{
-        await fetch(`${apiConfig.baseURL}/schedules`, {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ id, name, when})
-        })
+export async function scheduleNew({ id, name, when }) {
+  try {
+    // Faz a requisição para enviar os dados do agendamento
+    await fetch(`${apiConfig.baseURL}/schedules`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id, name, when }),
+    });
 
-        alert("Agendamento realizado!")
-    } catch (error){
-        console.log(error)
-        alert("Não foi possível agendar!")
-    }
+    alert("Agendamento realizado!");
+  } catch (error) {
+    console.log(error);
+    alert("Não foi possível agendar!");
+  }
 }
